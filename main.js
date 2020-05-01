323cbdabfad336998cf479f706e65068899338b7
Zach Roark <zach.roark@yahoo.com>
2020-05-01 10:32:40 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+let painter = {
+    tools: ["brush", "roller", "caulk", "sandpaper", "dropcloth"],
+    uniform: "white overalls",
+    cost_per_hour: 25.00,
+    insured: true
+  }
+//   Add an action that the painter can perform 
+//   ( a property whose value is a function ).
+// 
+//   Have her take that action by execucting the 
+//   method you created.
+
+//   BONUS: Add a method that allows us to add new tools for the painter.
